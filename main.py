6acaf86182d6e58e2aeceec4b99609d4055ca1ea
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-05-30 13:35:25 +0200

    Define episodes and reset environment

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import gymnasium as gym
 
 # A1.2 Create Breakout environment
 env = gym.make('BreakoutDeterministic-v4')
+
+# A1.3 Reset environment
+episodes = 50000
+
+for i in range(episodes):
+    observation = env.reset()
+
+
