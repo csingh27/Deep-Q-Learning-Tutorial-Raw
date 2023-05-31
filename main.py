7195a9cb5f1fd78cc3bd4556038eeeecefb236d4
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-05-31 10:49:59 +0200

    Add comments to define activities

--- main.py
+++ main.py
@@ -7,31 +7,40 @@ import gymnasium as gym
 env = gym.make('BreakoutDeterministic-v4')
 
 # A1.3 Reset environment
-episodes = 50000
+episodes = 1
 
 # A2 Frame pre-processing
 import cv2
 import numpy as np
 def frame_preprocess(obs):
 
+    # A2.1 Define the new height and width
     new_width = 110
     new_height = 84
 
+    # A2.2 Define frame as array
     frame = np.asarray(obs[0])
 
+    # A2.3 Convert frame to grayscale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # A2.4 Resize frame
     downsampled_frame = cv2.resize(gray_frame, (110, 84))
 
+    # A2.5 Crop frame to 84x84
     state = downsampled_frame[:,13:97]
     
     return state
 
+# A1.5 Define RL loop
 for i in range(episodes):
 
+    # A1.6 Reset environment
     observation = env.reset()
 
+    # A2 Frame pre-processing
     state = frame_preprocess(observation)
+    print(state.shape)
 
     cv2.imshow("Frame", state)
     cv2.waitKey(0)
@@ -43,7 +52,14 @@ for i in range(episodes):
     # 2 RIGHT
     # 3 LEFT
 
+    # A1.8 Define random action
     action = env.action_space.sample()
 
     while not done:
+        # A1.9 Take step
         state, reward, done, truncate, info = env.step(action)
+
+        # A2.6 Preprocess frame
+        next_state = frame_preprocess(state)
+        print(next_state.shape)
+        exit()
